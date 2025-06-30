e0111e0b30cab8eb08e0400ffa8136f77b74b9f7
saro-dev <codersaro@gmail.com>
2025-06-30 14:52:13 +0530

    replaced hero-bg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { Gift, Mail, Menu, MessageCircle, X } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useNavigate } from "react-router-dom";
-import FolkLore from '../assets/Folklore-logo.png';
+import FolkLore from "../assets/Folklore-logo.png";
 
-import { FloatingWhatsApp } from 'react-floating-whatsapp'
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 // import 'react-floating-whatsapp/dist/index.css'
 
 const navLinks = ["Gallery", "About", "Contact"];
@@ -23,7 +23,6 @@ const Header = () => {
         >
           <img src={FolkLore} alt="Folklore Logo" className="h-10 md:h-20" />
           <div className="">
-
             <h2 className="noto text-3xl md:text-4xl font-medium">FOLKLORE</h2>
             <h3 className="noto text-2xl md:text-3xl ">PRODUCTIONS</h3>
             {/* <small className="font-sans text-[12px]">TURNING YOUR RAW FOOTAGE INTO TIMESS FOLKTALES</small> */}
@@ -87,15 +86,38 @@ const Header = () => {
             </motion.div>
           )}
         </AnimatePresence>
-
       </header>
       <div class="flex flex-col md:flex-row justify-around items-center text-sm md:text-base bg-darkBrown py-2 px-4 gap-2 md:gap-0">
-        <span className="flex items-center text-accent"><MessageCircle size={28} className="bg-gray-300 p-1 rounded-full text-black mr-1" /> Whatsapp us: <a href="https://wa.me/917011584331" class=" font-medium">+91 7011584331</a></span>
-        <span className="hidden md:flex items-center text-accent"><Gift size={28} className="bg-gray-300 p-1 rounded-full text-black mr-1" /> Get flat 10% off on your first project</span>
-        <span className="flex items-center text-accent"><Mail size={28} className="bg-gray-300 p-1 rounded-full text-black mr-1" /> Mail us: <a href="mailto:contact@folklore.com" class=" font-medium">contact@folklore.com</a></span>
+        <span className="flex items-center text-accent">
+          <MessageCircle
+            size={28}
+            className="bg-gray-300 p-1 rounded-full text-black mr-1"
+          />{" "}
+          Whatsapp us:{" "}
+          <a href="https://wa.me/917011584331" class=" font-medium">
+            +91 7011584331
+          </a>
+        </span>
+        <span className="hidden md:flex items-center text-accent">
+          <Gift
+            size={28}
+            className="bg-gray-300 p-1 rounded-full text-black mr-1"
+          />{" "}
+          Get flat 10% off on your first project
+        </span>
+        <span className="flex items-center text-accent">
+          <Mail
+            size={28}
+            className="bg-gray-300 p-1 rounded-full text-black mr-1"
+          />{" "}
+          Mail us:{" "}
+          <a href="mailto:contact@folklore.com" class=" font-medium">
+            contact@folklore.com
+          </a>
+        </span>
       </div>
 
-          <FloatingWhatsApp
+      <FloatingWhatsApp
         phoneNumber="917011584331" // Replace with your number without + or spaces
         accountName="Folklore Productions"
         avatar={FolkLore} // Place logo inside public/ folder
